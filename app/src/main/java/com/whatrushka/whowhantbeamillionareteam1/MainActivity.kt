372db229ed78ad4e152378aee03316e98b07822b
Werr2787 <108471157+Werr2787@users.noreply.github.com>
2024-06-11 12:08:45 +0400

    commit

---
+++ app/src/main/java/com/whatrushka/whowhantbeamillionareteam1/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.whatrushka.whowhantbeamillionareteam1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.whatrushka.whowhantbeamillionareteam1.buisness.view_model.QuestionsViewModel
+import com.whatrushka.whowhantbeamillionareteam1.ui.theme.WhoWhantBeAMillionareTeam1Theme
+import com.whatrushka.whowhantbeamillionareteam1.views.navigation.Navigation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WhoWhantBeAMillionareTeam1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.background),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+
+                    Navigation(
+                        viewModel = QuestionsViewModel(applicationContext),
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
